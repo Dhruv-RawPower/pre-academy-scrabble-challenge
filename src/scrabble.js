771062653855e00786d65fd8d69ec24b088573f7
Dhruv Shah <dhruv10.shah@gmail.comm>
2023-05-17 11:57:53 +0530

    Successful implementation of the function score()

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,7 +1,33 @@
 class Scrabble {
+  constructor(word) {
+    //As the test uses a parameter to initialize the object of Scrabble, a parameterised constructor has been provided.
+    this.word = word ? word.toUpperCase() : "";
+  }
 
   score() {
-    // Write your implementation here
+    let score = 0;
+    
+    for (let i = 0; i < this.word.length; i++) {
+      const letter = this.word[i];
+      
+      if ('AEIOULNRST'.includes(letter)) {
+        score += 1;
+      } else if ('DG'.includes(letter)) {
+        score += 2;
+      } else if ('BCMP'.includes(letter)) {
+        score += 3;
+      } else if ('FHVWY'.includes(letter)) {
+        score += 4;
+      } else if (letter === 'K') {
+        score += 5;
+      } else if ('JX'.includes(letter)) {
+        score += 8;
+      } else if ('QZ'.includes(letter)) {
+        score += 10;
+      }
+    }
+    
+    return score;
   }
 }
 
